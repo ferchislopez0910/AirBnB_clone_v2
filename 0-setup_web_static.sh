29631f70ffed49006e4e2fb41c0df8db590cc8a7
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-08-26 19:15:28 -0500

    update: correct task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,21 +1,10 @@
 #!/usr/bin/env bash
 # Install Nginx already installed
-# Write a Bash script that sets up your web servers for the deployment of web_static. It must:
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-echo "
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-
-ln -sf /data/web_static/current /data/web_static/releases/test/
-chown -R ubuntu:ubuntu /data/
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "Test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
